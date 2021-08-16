ad1e19c78fd42d521051f4069662edbbd40f929e
koraykural <koraykural99@gmail.com>
2021-08-16 17:58:46 +0300

    products page, more features

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,10 +5,11 @@ import { ShopRoutingModule } from './shop-routing.module';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
 import { SharedModule } from '../shared/shared.module';
+import { RatingModule } from 'ng-starrating';
 import { ProductPreviewComponent } from './product-preview/product-preview.component';
 
 @NgModule({
   declarations: [ProductComponent, CartComponent, ProductPreviewComponent],
-  imports: [CommonModule, ShopRoutingModule, SharedModule],
+  imports: [CommonModule, ShopRoutingModule, SharedModule, RatingModule],
 })
 export class ShopModule {}
