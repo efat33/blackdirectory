e4dc7740d64c024c927f580a0b66eafbc7e40fe1
Zakaria Ahmed <zakaria.buet13@gmail.com>
2021-08-24 22:59:11 +0600

    Fix responsive of Mobiles pages

--- src/app/listing/listing.module.ts
+++ src/app/listing/listing.module.ts
@@ -10,11 +10,12 @@ import { ListingPlanComponent } from "./listing-plan/listing-plan.component";
 import { ListingNewComponent } from "./listing-new/listing-new.component";
 import { ListingEditComponent } from './listing-edit/listing-edit.component';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { ProductPreviewComponent } from '../shop/product-preview/product-preview.component';
 
 
 @NgModule({
-  declarations: [ListingSearchComponent, ListingDetailsComponent, ListingPlanComponent, ListingNewComponent, 
-      ListingEditComponent],
+  declarations: [ListingSearchComponent, ListingDetailsComponent, ListingPlanComponent, ListingNewComponent,
+      ListingEditComponent, ProductPreviewComponent],
   imports: [CommonModule, SharedModule, ListingRoutingModule, CKEditorModule]
 })
 export class ListingModule {}
